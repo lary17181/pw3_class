a4ff00edd84a494acf1cdcea9a419218214a3aba
unknown <lary.nany17@gmail.com>
2025-05-06 10:54:48 -0300

    Forms

---
+++ pw3-Forms/src/app/calculadora/calculadora.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-calculadora',
+  imports: [ReactiveFormsModule],
+  templateUrl: './calculadora.component.html',
+  styleUrl: './calculadora.component.css'
+})
+export class CalculadoraComponent {
+Form:FormGroup;
+
+constructor(private fb: FormBuilder){
+  this.Form = this.fb.group({
+  num1 : this.fb.control(0),
+  num2 : this.fb.control(0)
+});
+}
+
+
+
+  resultado:number = 0
+  Calcular(){
+  let num1 =this.Form.get('num1')?.value;
+  let num2 =this.Form.get('num2')?.value;
+  this.resultado = num1 + num2;
+}
+}
